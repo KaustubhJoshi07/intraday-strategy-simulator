d6df2db567b82e8c20f9e4d6a4b0826bb1285342
Kaustubh Joshi <kjoshi542@gmail.com>
2026-04-13 21:46:48 -0400

    Update app title to Intraday Strategy Sim

--- backend/app/main.py
+++ backend/app/main.py
@@ -6,11 +6,7 @@ app = FastAPI(title="Mini Quant Backtester API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:5173",
-        "http://127.0.0.1:5173",
-        "https://intraday-strategy-sim.onrender.com",
-    ],
+    allow_origins=["*"],  # TEMP FIX
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
